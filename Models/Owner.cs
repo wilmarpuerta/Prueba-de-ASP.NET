a4c3b38dec0e225b79458fa65ad790734906a1d3
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-06-04 17:26:30 -0500

    Modificacion

--- Models/Owner.cs
+++ Models/Owner.cs
@@ -1,14 +1,24 @@
 
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
 namespace Prueba_de_ASP.NET.Models
 {
     public class Owner
     {
         public int Id { get; set; }
+        [Required]
         public string Names { get; set; }
+        [Required]
         public string LastNames { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string Phone { get; set; }
+        [JsonIgnore]
+        public List<Pet> Pets { get; set; }
         
     }
 }
