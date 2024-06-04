41bdd3a8e5e6b204eac7aa28fc0db57889e32ecc
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-06-04 16:01:53 -0500

    Agregando los servicios de Owners

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Prueba_de_ASP.NET.Data;
+using Prueba_de_ASP.NET.Services.Owners;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<IOwnersRepository, OwnersRepository>();
 
 builder.Services.AddDbContext<BaseContext> (options => 
     options.UseMySql(
@@ -43,6 +47,8 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+
+app.MapControllers();
 
 app.Run();
 
