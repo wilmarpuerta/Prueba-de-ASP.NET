b78493d0ce1b752b55a2c503fc860676c2ca4be4
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-06-04 20:35:17 -0500

    Actualizacion de modelo

--- Models/Vet.cs
+++ Models/Vet.cs
@@ -1,12 +1,18 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Prueba_de_ASP.NET.Models
 {
     public class Vet
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Phone { get; set; }
+        [Required]
         public string Address { get; set; }
+        [Required]
         public string Email { get; set; }
     }
 }
