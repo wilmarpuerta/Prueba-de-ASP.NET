9f8be9b4dce2528d4e5ba268800ebfaf9123b687
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-06-04 21:04:13 -0500

    Actualización de modelos

--- Models/Quote.cs
+++ Models/Quote.cs
@@ -1,12 +1,18 @@
 
+using System.ComponentModel.DataAnnotations;
+
 namespace Prueba_de_ASP.NET.Models
 {
     public class Quote
     {
         public int Id { get; set; }
+        [Required]
         public DateTime Date { get; set; }
+        [Required]
         public int PetId { get; set; }
+        [Required]
         public int VetId { get; set; }
+        [Required]
         public string Description { get; set; }
     }
 }
